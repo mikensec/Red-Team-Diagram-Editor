9bd82d329bcf053c833a23ed3310d9aa974f118e
Michael Nieto <michaelnietotx@gmail.com>
2025-11-18 09:32:12 -0600

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Red-Team-Diagram-Editor/sw.js', { scope: '/Red-Team-Diagram-Editor/' })})}
